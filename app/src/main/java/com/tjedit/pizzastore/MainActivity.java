c86236cf63c0695b8f4c0a144d80d9e47d1b6c48
user <kyronko@naver.com>
2019-04-28 16:02:26 +0900

    정보재입력

--- app/src/main/java/com/tjedit/pizzastore/MainActivity.java
+++ app/src/main/java/com/tjedit/pizzastore/MainActivity.java
@@ -28,14 +28,20 @@ public class MainActivity extends AppCompatActivity{
         pizzaAdapter = new PizzaAdapter(MainActivity.this, pizzaData);
         act.pizzaList.setAdapter(pizzaAdapter);
 
+
         fill();
 
     }
+
+
     void fill(){
-        pizzaData.add(new PizzaData("도미넛피자","09:00~21:00","https://mblogthumb-phinf.pstatic.net/20141124_182/howtomarry_1416806028308979cg_PNG/Pizza_Hut_logo.svg.png?type=w2"));
-        pizzaData.add(new PizzaData("피자헛피자","09:00~21:00","https://mblogthumb-phinf.pstatic.net/20141124_182/howtomarry_1416806028308979cg_PNG/Pizza_Hut_logo.svg.png?type=w2"));
-        pizzaData.add(new PizzaData("맥시코피자","09:00~21:00","https://mblogthumb-phinf.pstatic.net/20141124_182/howtomarry_1416806028308979cg_PNG/Pizza_Hut_logo.svg.png?type=w2"));
-        pizzaData.add(new PizzaData("파파피자","09:00~21:00","https://mblogthumb-phinf.pstatic.net/20141124_182/howtomarry_1416806028308979cg_PNG/Pizza_Hut_logo.svg.png?type=w2"));
+        pizzaData.add(new PizzaData("파파존스", "09:00~21:00", "010-1234-5678", "http://postfiles2.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w966"));
+        pizzaData.add(new PizzaData("피자헛", "09:00~21:00", "010-1234-5678", "https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Pizza_Hut_logo.svg/220px-Pizza_Hut_logo.svg.png"));
+        pizzaData.add(new PizzaData("도미노피자", "09:00~21:00", "010-1234-5678", "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Domino%27s_pizza_logo.svg/120px-Domino%27s_pizza_logo.svg.png"));
+        pizzaData.add(new PizzaData("미스터피자", "09:00~21:00", "010-1234-5678", "http://postfiles12.naver.net/20160530_171/ppanppane_14646177044221JRNd_PNG/%B9%CC%BD%BA%C5%CD%C7%C7%C0%DA_%B7%CE%B0%ED_%281%29.png?type=w966"));
+        pizzaData.add(new PizzaData("피자에땅", "09:00~21:00", "010-1234-5678", "https://mblogthumb-phinf.pstatic.net/20160530_214/ppanppane_1464617804922knKn4_PNG/%C7%C7%C0%DA%BF%A1%B6%A5_%B7%CE%B0%ED_%282%29.png?type=w800"));
+        pizzaData.add(new PizzaData("피자스쿨", "09:00~21:00", "010-1234-5678", "https://modo-phinf.pstatic.net/20150501_269/1430484184544WKwLF_JPEG/mosa7NPaR2.jpeg?type=f320_320"));
+        pizzaData.add(new PizzaData("피자나라 치킨공주", "09:00~21:00", "010-1234-5678", "http://www.newsworker.co.kr/news/photo/201810/22592_19806_5940.jpg"));
 
     }
 }
--- app/src/main/java/com/tjedit/pizzastore/datas/PizzaData.java
+++ app/src/main/java/com/tjedit/pizzastore/datas/PizzaData.java
@@ -3,11 +3,13 @@ package com.tjedit.pizzastore.datas;
 public class PizzaData {
     public String pizzaStoreNameTxt;
     public  String openTimeTxt;
+    public  String numberTxt;
     public String imageURL;
 
-    public PizzaData(String pizzaStoreNameTxt, String openTimeTxt, String imageURL) {
+    public PizzaData(String pizzaStoreNameTxt, String openTimeTxt, String numberTxt, String imageURL) {
         this.pizzaStoreNameTxt = pizzaStoreNameTxt;
         this.openTimeTxt = openTimeTxt;
+        this.numberTxt = numberTxt;
         this.imageURL = imageURL;
     }
 }
